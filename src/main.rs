93e2e278cdc329e443dd1b4df7b5ea1b81ddbcf3
Khinshan Khan <shan@uptogood.dev>
2025-12-24 17:47:59 -0500

    log epic data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod epic;
+
+use epic::EpicResponse;
+
 fn get_epic_data() -> Result<String, reqwest::Error> {
     let epic_url =
         "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US";
@@ -5,9 +9,27 @@ fn get_epic_data() -> Result<String, reqwest::Error> {
     Ok(reqwest::blocking::get(epic_url)?.text()?)
 }
 
+fn handle_epic() -> Result<(), Box<dyn std::error::Error>> {
+    let body = get_epic_data()?;
+    let root = serde_json::from_str::<EpicResponse>(&body)?;
+
+    let offers = root.data.catalog.search_store.elements;
+    for offer in offers.iter() {
+        println!(
+            "{} ({}:{}) discountPrice={}",
+            offer.title,
+            offer.namespace,
+            offer.id,
+            offer.price.total_price.discount_price
+        );
+    }
+
+    Ok(())
+}
+
 fn main() {
-    match get_epic_data() {
-        Ok(data) => println!("Epic Games Data: {}", data),
-        Err(e) => eprintln!("Error fetching data: {}", e),
+    match handle_epic() {
+        Ok(()) => println!("Successfully fetched and displayed Epic Games offers."),
+        Err(e) => eprintln!("HTTP error: {e}"),
     }
 }
