6b303bd3857d81719b661feb6861906182c2bed2
Khinshan Khan <shan@uptogood.dev>
2025-12-24 12:48:53 -0500

    get epic data as string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn get_epic_data() -> Result<String, reqwest::Error> {
+    let epic_url =
+        "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US";
+
+    match reqwest::blocking::get(epic_url) {
+        Ok(response) => {
+            match response.text() {
+                Ok(text) => Ok(text),
+                Err(e) => Err(e),
+            }
+        }
+        Err(e) => Err(e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    match get_epic_data() {
+        Ok(data) => println!("Epic Games Data: {}", data),
+        Err(e) => eprintln!("Error fetching data: {}", e),
+    }
 }
