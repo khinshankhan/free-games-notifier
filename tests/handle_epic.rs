d1281f140a75f2eb8e4517cad244612165d9ac63
Khinshan Khan <shan@uptogood.dev>
2026-01-03 16:42:55 -0500

    single promo test

--- tests/handle_epic.rs
+++ tests/handle_epic.rs
@@ -4,11 +4,18 @@ use free_games_notifier::{app, epic, notifier, offer_store, time};
 #[cfg(test)]
 mod fixture_tests {
     use super::*;
-    #[test]
-    fn test_handle_epic() {
-        let ts = time::MockTimeSource::new(time::parse_utc("2026-01-02T16:00:00.000Z").unwrap());
 
-        let resp = include_str!("./fixtures/epic_multiple_promo.json");
+    fn setup(
+        s: &str,
+        resp: &str,
+    ) -> (
+        time::MockTimeSource,
+        epic::http::MockClient,
+        offer_store::SqliteOfferStore,
+        notifier::CaptureNotifier,
+    ) {
+        let ts = time::MockTimeSource::new(time::parse_utc(s).unwrap());
+
         let ec = epic::http::MockClient::new(resp.to_string());
 
         let db = rusqlite::Connection::open_in_memory().unwrap();
@@ -17,14 +24,44 @@ mod fixture_tests {
 
         let n = notifier::CaptureNotifier::new();
 
+        (ts, ec, offer_store, n)
+    }
+
+    #[test]
+    fn test_handle_epic_single_promo() {
+        let (ts, ec, offer_store, n) = setup(
+            "2025-12-20T16:15:00.000Z",
+            include_str!("./fixtures/epic_single_promo.json"),
+        );
+
+        app::handle_epic(&ts, &ec, &offer_store, &n).unwrap();
+
+        let msgs: std::collections::HashSet<String> = n.get_messages().into_iter().collect();
+        let expected: std::collections::HashSet<String> = [
+            "**Blood West** is now free on EGS! Ends <t:1766332800:R>\nhttps://www.epicgames.com/store/en-US/p/blood-west-8f6ffd"
+        ]
+        .into_iter()
+        .map(String::from)
+        .collect();
+
+        assert_eq!(msgs, expected);
+    }
+
+    #[test]
+    fn test_handle_epic_multiple_promo() {
+        let (ts, ec, offer_store, n) = setup(
+            "2026-01-01T16:15:00.000Z",
+            include_str!("./fixtures/epic_multiple_promo.json"),
+        );
+
         app::handle_epic(&ts, &ec, &offer_store, &n).unwrap();
 
         let msgs: std::collections::HashSet<String> = n.get_messages().into_iter().collect();
         let expected: std::collections::HashSet<String> = [
             "**Wildgate** is now free on EGS! Ends <t:1767888000:R>\n\
-https://www.epicgames.com/store/en-US/p/wildgate-standard-edition-b886b5",
+             https://www.epicgames.com/store/en-US/p/wildgate-standard-edition-b886b5",
             "**Total War: THREE KINGDOMS** is now free on EGS! Ends <t:1767888000:R>\n\
-https://www.epicgames.com/store/en-US/p/total-war-three-kingdoms-d3bb7a",
+             https://www.epicgames.com/store/en-US/p/total-war-three-kingdoms-d3bb7a",
         ]
         .into_iter()
         .map(String::from)
